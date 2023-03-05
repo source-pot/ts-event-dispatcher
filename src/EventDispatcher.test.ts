60cc32c0d81a42aeae46aa70ad88984b8a6324d0
Rob Watson <rob.watson@me.com>
2023-03-05 09:45:04 +0000

    Achieve 100% test coverage

--- src/EventDispatcher.test.ts
+++ src/EventDispatcher.test.ts
@@ -1,4 +1,4 @@
-import { createEvent } from './EventDispatcher'
+import { createEvent, createEventDispatcher } from './EventDispatcher'
 
 test('can create event', () => {
   expect(createEvent('test', {}))
@@ -17,3 +17,56 @@ test('can attach payload to event', () => {
       data: 'test'
     });
 });
+
+test('can create dispatcher with no default handlers', () => {
+  const dispatcher = createEventDispatcher()
+  expect(dispatcher).toHaveProperty('on')
+  expect(dispatcher).toHaveProperty('emit')
+})
+
+test('can create dispatcher with default handlers', () => {
+  const dispatcher = createEventDispatcher([
+    ['test', () => {}]
+  ])
+  expect(dispatcher).toHaveProperty('on')
+  expect(dispatcher).toHaveProperty('emit')
+})
+
+test('can unsubscribe from event', () => {
+  const handler = jest.fn(event => {})
+  const eventName = 'test'
+  const dispatcher = createEventDispatcher()
+  const unsub = dispatcher.on(eventName, handler)
+
+  unsub()
+  dispatcher.emit(createEvent('test', {}))
+
+  expect(handler).toBeCalledTimes(0)
+})
+
+test('emit correctly calls handler', () => {
+  const handler = jest.fn(event => {})
+
+  const dispatcher = createEventDispatcher([
+    ['test', handler]
+  ])
+
+  dispatcher.emit(createEvent('test', {}))
+
+  expect(handler.mock.calls).toHaveLength(1)
+})
+
+test('emit event can be cancelled', () => {
+  const handler = jest.fn(event => { event.cancelled = true })
+  const handlerThatShouldNotBeCalled = jest.fn(event => {})
+
+  const dispatcher = createEventDispatcher([
+    ['test', handler],
+    ['test', handlerThatShouldNotBeCalled]
+  ])
+
+  dispatcher.emit(createEvent('test', {}))
+
+  expect(handler).toHaveBeenCalledTimes(1)
+  expect(handlerThatShouldNotBeCalled).toHaveBeenCalledTimes(0)
+})
